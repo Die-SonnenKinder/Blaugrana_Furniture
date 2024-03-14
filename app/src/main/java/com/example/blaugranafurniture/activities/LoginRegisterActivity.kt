0183a84a3d7156509b28cb65aa2c1b94c88ffdae
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-03-14 15:03:47 +0200

    project init, setup, add dependency, fix bug

---
+++ app/src/main/java/com/example/blaugranafurniture/activities/LoginRegisterActivity.kt
@@ -0,0 +1,12 @@
+package com.example.blaugranafurniture.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.blaugranafurniture.R
+
+class LoginRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login_register)
+    }
+}
---
+++ build.gradle.kts
@@ -0,0 +1,17 @@
+buildscript {
+    repositories {
+        google()
+    }
+    dependencies {
+        val nav_version = "2.7.7"
+        classpath("androidx.navigation:navigation-safe-args-gradle-plugin:$nav_version")
+        classpath("com.google.dagger:hilt-android-gradle-plugin:2.51")
+    }
+}
+
+
+plugins {
+    id("com.android.application") version "8.2.1" apply false
+
+    id("org.jetbrains.kotlin.android") version "1.9.0" apply false
+}
