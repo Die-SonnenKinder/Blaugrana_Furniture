23f6c27916cfe7eb10a4d30897bf72840dbb4963
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-03-15 02:00:02 +0200

    Done Introduction screen UI

---
+++ app/src/main/java/com/example/blaugranafurniture/fragments/loginRegister/IntroductionFragment.kt
@@ -0,0 +1,7 @@
+package com.example.blaugranafurniture.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.blaugranafurniture.R
+
+class IntroductionFragment: Fragment(R.layout.fragment_introduction) {
+}
---
+++ app/src/main/java/com/example/blaugranafurniture/fragments/loginRegister/LoginFragment.kt
@@ -0,0 +1,7 @@
+package com.example.blaugranafurniture.fragments.loginRegister
+
+import androidx.fragment.app.Fragment
+import com.example.blaugranafurniture.R
+
+class LoginFragment: Fragment(R.layout.fragment_login) {
+}
