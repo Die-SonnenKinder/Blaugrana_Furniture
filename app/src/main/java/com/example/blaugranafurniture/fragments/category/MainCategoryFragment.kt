e39c66b8690f1f3709aa1f73024931cc47591414
Hoang <jackthinhphan@gmail.com>
2024-03-24 12:35:01 +0200

    innerUI

---
+++ app/src/main/java/com/example/blaugranafurniture/fragments/category/MainCategoryFragment.kt
@@ -0,0 +1,6 @@
+package com.example.blaugranafurniture.fragments.category
+
+import androidx.fragment.app.Fragment
+import com.example.blaugranafurniture.R
+class MainCategoryFragment: Fragment(R.layout.fragment_main_category) {
+}
