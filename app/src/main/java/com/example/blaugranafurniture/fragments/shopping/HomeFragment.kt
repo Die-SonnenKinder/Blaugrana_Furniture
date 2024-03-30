e641bce471d591fe4837303bdfd862f622b9bccc
Hoang <jackthinhphan@gmail.com>
2024-03-30 19:10:23 +0200

    BestProductsandDeals

--- app/src/main/java/com/example/blaugranafurniture/fragments/shopping/HomeFragment.kt
+++ app/src/main/java/com/example/blaugranafurniture/fragments/shopping/HomeFragment.kt
@@ -41,6 +41,8 @@ class HomeFragment: Fragment(R.layout.fragment_home) {
             BathroomFragment(),
         )
 
+        binding.viewpagerHome.isUserInputEnabled = false
+
         val viewPager2Adapter = HomeviewpagerAdapter(categoriesFragments,childFragmentManager,lifecycle)
         binding.viewpagerHome.adapter = viewPager2Adapter
         TabLayoutMediator(binding.tabLayout, binding.viewpagerHome) { tab, position ->
--- app/src/main/java/com/example/blaugranafurniture/viewmodel/MainCategoryViewModel.kt
+++ app/src/main/java/com/example/blaugranafurniture/viewmodel/MainCategoryViewModel.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
-import kotlin.math.log
 
 @HiltViewModel
 class MainCategoryViewModel @Inject constructor(
@@ -20,8 +19,16 @@ class MainCategoryViewModel @Inject constructor(
     private val _specialProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
     val specialProducts: StateFlow<Resource<List<Product>>> = _specialProducts
 
+    private val _bestDealsProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val bestDealsProducts: StateFlow<Resource<List<Product>>> = _bestDealsProducts
+
+    private val _bestProducts = MutableStateFlow<Resource<List<Product>>>(Resource.Unspecified())
+    val bestProducts: StateFlow<Resource<List<Product>>> = _bestProducts
+
     init {
         fetchSpecialProducts()
+        fetchBestDeals()
+        fetchBestProducts()
     }
 
     fun fetchSpecialProducts() {
@@ -38,6 +45,41 @@ class MainCategoryViewModel @Inject constructor(
             }.addOnFailureListener {
                 viewModelScope.launch {
                     _specialProducts.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun fetchBestDeals () {
+        viewModelScope.launch {
+            _bestDealsProducts.emit(Resource.Loading())
+        }
+        firestore.collection("products")
+            .whereEqualTo("category", "Best Deals").get()
+            .addOnSuccessListener { result ->
+                val bestDealsProducts = result.toObjects(Product::class.java)
+                viewModelScope.launch {
+                    _bestDealsProducts.emit(Resource.Success(bestDealsProducts))
+                }
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _bestDealsProducts.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    fun fetchBestProducts () {
+        viewModelScope.launch {
+            _bestProducts.emit(Resource.Loading())
+        }
+        firestore.collection("products").get()
+            .addOnSuccessListener { result ->
+                val bestProducts = result.toObjects(Product::class.java)
+                viewModelScope.launch {
+                    _bestProducts.emit(Resource.Success(bestProducts))
+                }
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _bestProducts.emit(Resource.Error(it.message.toString()))
                 }
             }
     }
