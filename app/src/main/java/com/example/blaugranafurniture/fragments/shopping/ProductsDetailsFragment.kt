ab4855ccce09adff52ff9669a58787593dedbe24
Hoang <jackthinhphan@gmail.com>
2024-04-03 12:25:35 +0300

    productdetailframe

---
+++ app/src/main/java/com/example/blaugranafurniture/fragments/shopping/ProductsDetailsFragment.kt
@@ -0,0 +1,7 @@
+package com.example.blaugranafurniture.fragments.shopping
+
+import androidx.fragment.app.Fragment
+import com.example.blaugranafurniture.R
+
+class ProductsDetailsFragment: Fragment(R.layout.fragment_product_details) {
+}
