72428f4a7675a7a0cb0df0bc9fa12f0e7c1f4697
Dang Hoang Ha <ha.dh290702@gmail.com>
2024-04-13 13:25:56 +0300

    fix ViewModel bug

--- app/src/main/java/com/example/blaugranafurniture/viewmodel/AddressViewModel.kt
+++ app/src/main/java/com/example/blaugranafurniture/viewmodel/AddressViewModel.kt
@@ -6,6 +6,7 @@ import com.example.blaugranafurniture.data.Address
 import com.example.blaugranafurniture.util.Resource
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableSharedFlow
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asSharedFlow
@@ -13,6 +14,7 @@ import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
+@HiltViewModel
 class AddressViewModel @Inject constructor(
     private val firestore: FirebaseFirestore,
     private val auth: FirebaseAuth
